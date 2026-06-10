497c43e5a7d4a045cf48acc68ae55ee3f698537c
Jonathan Blok <guanothan@gmail.com>
2026-06-10 22:19:42 +0200

    Update criteria.py
    fixed a few issues preventing me from generating ontology from schema.org rdf

--- criteria.py
+++ criteria.py
@@ -1,9 +1,8 @@
 import rdflib
-from rdflib import Graph, URIRef, Namespace, util
+from rdflib import Graph, URIRef, Namespace, util, Literal
 from rdflib.util import SUFFIX_FORMAT_MAP
-from rdflib.namespace import NamespaceManager, RDFS, RDF, XSD, SKOS, SH
+from rdflib.namespace import RDFS, RDF, SDO, SKOS, SH
 import re
-import sys
 import argparse
 import json
 
@@ -134,8 +133,26 @@ def convert(dataGraph,conf):
 	stmtList = []
 	doubleInst = [] # to check for URI with double instantiations
 	i = 0
+
+	printgraph = Graph()
+
 	for s,p,o in g.triples((None, None, None)):
+		s_type = g.value(subject=s, predicate=RDF.type)
+		if p == RDF.type:
+			pass
+		elif type(o) == Literal:
+			if (s_type, p, Literal('Literal')) not in printgraph:
+				printgraph.add((s_type, p, Literal('Literal')))
+		else:
+			o_type = g.value(subject=o, predicate=RDF.type)
+			if o_type != None and (s_type, p, o_type) not in printgraph:
+				printgraph.add((s_type, p, o_type))
+			elif type(o) == URIRef and (s_type, p, o_type) not in printgraph:
+				printgraph.add((s_type, p, URIRef(o)))
+	
+	for s,p,o in printgraph.triples((None, None, None)):
 		p = p.n3(g.namespace_manager)
+
 		if s in uriDict:
 			n1 = uriDict[s]
 		else:
@@ -154,13 +171,14 @@ def convert(dataGraph,conf):
 
 		# check whether the object of the triple is a key in the returned dict of classDict
 		# to retrieve the Mermaid class, i.e check for the object of the property rdf:type
-		if p == 'rdf:type':
+		if p == RDF.type:
 			c = o.n3(g.namespace_manager)
 			if c in classes:
 				cl = classes[c]
 			else:
 				cl = 'Default'
-			if not s in doubleInst:
+			if (not s in doubleInst) and o != SDO.Dataset:
+				print(f'adding {s}')
 				doubleInst.append(s)
 				uriCl = cl+'_URI'
 			else:
@@ -230,7 +248,7 @@ def shapeProc(dataGraph,shapeInput):
 
 # Function to generate mermaid.jd style class definition based the classes dict from source.py
 def template(conf):
-	template = "flowchart TD\n"
+	template = '%%{init: {\"flowchart\": {\"defaultRenderer\": \"elk\"}} }%%\nflowchart TD\n'
 	if 'style' in conf:
 		classes = conf['style']
 	else:
@@ -296,17 +314,22 @@ def ontology(dataGraph, mmdOutput, conf):
 		stmt = stmt.replace('(["<','([').replace('>"])','])')
 
 		date = re.findall('\(\[".*\^xsd:dateTime"]\)', stmt)
-		lit = re.findall('\(\["\'\'.*\'\'.*"]\)', stmt)
+		lit = re.findall('\(\["\'\'.*\'\'.*"]\)', stmt, re.DOTALL)
+		term = re.findall('([https://.*])', stmt)
+					 
 		if date:
 			stmt = stmt.replace(date[0], '["xsd:dateTime"]')
 		elif lit:
 			stmt = stmt.replace(lit[0], '["xsd:string"]')
+		elif term:
+			stmt = stmt.replace(term[0], '["xsd:anyURI"]')
 		
 		if 'rdf:type' in stmt:
 			inst = re.findall('\(\[.*:.*\)', stmt)[0] # get the uri part
 			clType = '[' + re.findall('\|.*\[".*"\].*', stmt)[0].split('[')[1] # get the class part
 			# add to the uriType dict the uri part as the key, and the class part as the value
 			# so the class part will replace the uri part in the for loop below
+			
 			if not inst in uriType:
 				uriType[inst] = clType
 			# else if uri is already in uriType, indicating multi instantiations,
@@ -321,14 +344,16 @@ def ontology(dataGraph, mmdOutput, conf):
 			statements.append(stmt)
 
 	for stmt in statements:
-		m = re.match('\d+(\(\[.*:.*\)) -->\|(.*)\| (\d+)(\(*\[(.*)\]\)*)', stmt)
+		m = re.match('\d+(\(\[.*:.*\)) -->\|(.*)\| (\d+)(\(*\[(.*)\]\)*)', stmt, re.DOTALL)
 		label = ''
+		if m == None:
+			print(f'No match for: {stmt}')
 		if m.group(1) in uriType:
 			stmt = stmt.replace(m.group(1), uriType[m.group(1)])
 		if m.group(4) in uriType:
 			stmt = stmt.replace(m.group(4), uriType[m.group(4)])
 
-		m2 = re.match('\d+(\(*\[.*:.*\)*) -->\|(.*)\| (\d+)(\(*\[(.*)\]\)*)(.*)', stmt)
+		m2 = re.match('\d+(\(*\[.*:.*\)*) -->\|(.*)\| (\d+)(\(*\[(.*)\]\)*)(.*)', stmt, re.DOTALL)
 		mGroup1 = m.group(1).replace('"','')
 		if mGroup1 in nodeLabels and m.group(2) in nodeLabels[mGroup1]:
 			label = nodeLabels[mGroup1][m.group(2)]
